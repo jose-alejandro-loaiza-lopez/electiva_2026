d29826d3b2e950e9d45250f683d337c5b76018d4
dduranuceva <dduran@uceva.edu.co>
2026-03-23 10:58:44 -0500

    feat: Initialize Flutter project structure for Windows
    - Added CMakeLists.txt for project configuration.
    - Created Windows runner files including main application logic.
    - Implemented Win32 window management for Flutter integration.
    - Added resource files for application icon and versioning.
    - Included manifest file for web app configuration.
    - Established plugin registration mechanism for Flutter plugins.
    - Set up utility functions for command line argument handling and console management.

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: const Color.fromARGB(255, 240, 118, 5),
+        ),
+      ),
+      home: const MyHomePage(title: 'D.A.M UCEVA'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('CONTADOR:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment', //QUE ES EL TOOLTIP?
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
