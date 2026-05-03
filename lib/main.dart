3c2c17e0c52176532314a67e34b8d461b2bde71e
Jose Loaiza <jose.loaiza01@uceva.edu.co>
2026-05-03 13:05:08 -0500

    docx: version 1.0.1+1

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:electiva_2026/routes/app_router.dart';
 import 'package:flutter/material.dart';
-import 'themes/app_theme.dart'; // Importar el tema
+import 'themes/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
